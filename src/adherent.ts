39033c4dd48be7fa1c17add5f0ae95d0bd1e1307
Adeline64 <adeline.lacabanne@gmail.com>
2022-07-29 11:28:17 +0200

    adherent

--- src/adherent.ts
+++ src/adherent.ts
@@ -6,24 +6,38 @@ export class Adherent {
     private prenom : string;
     private budget : Budget;
     private role:string[] = [];
-    private telephone : string;
+    private telephone : number;
     private email : string;
+    private password : string | undefined;
 
 
     //constructor 
-    constructor(nom: string, prenom: string) {
+    constructor(nom: string, prenom: string, telephone:number, email:string, password:string) {
         this.id = Date.now.toString()
         this.nom = nom;
         this.prenom = prenom
         this.budget = new Budget(900,[])
         this.role = []
-        this.telephone = "telephone"
+        this.telephone = telephone
         this.email = "email"
+        this.password = password
     }
     
 
     create(){
-        
+        document.getElementById("form-save").addEventListener("click", function() {
+            // Récupération des champs
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+          
+            if (email && password) {
+              // Nouvelle ligne
+              const movie = { email: email, password: password };
+          
+              // Ajout de la nouvelle ligne
+              movies.push(movie);
+            }
+          });
     }
 
     read(){
